f34971ef0e42c53d233facdd0bc890fefdff24fb
chosangho <hyundai_sangho@naver.com>
2022-01-23 23:24:00 +0900

    first commit

---
+++ src/Something.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onDestroy, onMount, beforeUpdate, afterUpdate } from "svelte";
+
+  beforeUpdate(() => {
+    console.log("before update");
+  });
+
+  afterUpdate(() => {
+    console.log("after update");
+  });
+
+  onMount(() => {
+    console.log("Mounted");
+  });
+
+  onDestroy(() => {
+    console.log("Before destroy");
+  });
+</script>
+
+<h1>Something</h1>
